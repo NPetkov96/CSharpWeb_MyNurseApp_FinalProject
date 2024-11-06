853718ff448760a51f1445def70ba23ad9d0955a
NPetkov96 <126357169+NPetkov96@users.noreply.github.com>
2024-11-06 11:56:51 +0200

    Add project files.

---
+++ MyNurseApp.Data.Models/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MyNurseApp.Data.Models
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ MyNurseApp.Web.ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyNurseApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
