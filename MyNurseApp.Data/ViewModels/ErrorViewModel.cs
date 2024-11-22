5d4edf865937fd1b825014b95b519a5437d1c56e
NPetkov96 <126357169+NPetkov96@users.noreply.github.com>
2024-11-22 18:01:14 +0200

    s

--- MyNurseApp.Data/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyNurseApp.Data.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
