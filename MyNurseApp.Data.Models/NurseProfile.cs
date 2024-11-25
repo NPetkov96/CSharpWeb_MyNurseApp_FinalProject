1ef816c033d66981c1867d7181bc3962b0ec541f
NPetkov96 <126357169+NPetkov96@users.noreply.github.com>
2024-11-25 22:37:03 +0200

    Manipulation section updated

--- MyNurseApp.Data.Models/NurseProfile.cs
+++ MyNurseApp.Data.Models/NurseProfile.cs
@@ -6,39 +6,39 @@ namespace MyNurseApp.Data
 {
     public class NurseProfile
     {
-        //[Key]
-        //public Guid Id { get; set; }
+        [Key]
+        public Guid Id { get; set; }
 
-        //[Required]
-        //[StringLength(NurseConstants.NurseMaxNameLength)]
-        //[Comment("First name of the nurse")]
-        //public string FirstName { get; set; } = null!;
+        [Required]
+        [StringLength(99)]
+        [Comment("First name of the nurse")]
+        public string FirstName { get; set; } = null!;
 
-        //[Required]
-        //[StringLength(NurseConstants.NurseMaxNameLength)]
-        //[Comment("Last name of the nurse")]
-        //public string LastName { get; set; } = null!;
+        [Required]
+        [StringLength(99)]
+        [Comment("Last name of the nurse")]
+        public string LastName { get; set; } = null!;
 
-        //[Required]
-        //public string Email { get; set; } = null!;
+        [Required]
+        public string Email { get; set; } = null!;
 
-        //[Required]
-        //[StringLength(NurseConstants.NursePhoneNumberMaxLength)]
-        //public string PhoneNumber { get; set; } = null!;
+        [Required]
+        [StringLength(13)]
+        public string PhoneNumber { get; set; } = null!;
 
-        //[Required]
-        //[Comment("Min years of experience for this work")]
-        //public int YearsOfExperience { get; set; } 
+        [Required]
+        [Comment("Min years of experience for this work")]
+        public int YearsOfExperience { get; set; }
 
-        //public string? Recommendations { get; set; }
+        public string? Recommendations { get; set; }
 
-        //[Required]
-        //public string Education { get; set; } = null!;
+        [Required]
+        public string Education { get; set; } = null!;
 
-        //public Guid UserId { get; set; }
+        public Guid UserId { get; set; }
 
-        //public ApplicationUser User { get; set; } = null!;
+        public ApplicationUser User { get; set; } = null!;
 
-        //public bool IsDeleted { get; set; } = false;
+        public bool IsDeleted { get; set; } = false;
     }
 }
--- MyNurseApp.Web.ViewModels/PatientProfile/PatientProfileViewModel.cs
+++ MyNurseApp.Web.ViewModels/PatientProfile/PatientProfileViewModel.cs
@@ -1,6 +1,4 @@
-﻿
-using MyNurseApp.Common.Constants;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MyNurseApp.Web.ViewModels.PatientProfile
 {
@@ -9,39 +7,39 @@ namespace MyNurseApp.Web.ViewModels.PatientProfile
         public Guid Id { get; set; }
 
         [Required(ErrorMessage = "First name is required.")]
-        [MinLength(PatientConstants.PatientMinNameLength, ErrorMessage = $"First name must be at least 3 characters long")]
-        [MaxLength(PatientConstants.PatientMaxNameLength, ErrorMessage = $"First name must not exceed 99 characters.")]
+        [MinLength(3, ErrorMessage = $"First name must be at least 3 characters long")]
+        [MaxLength(99, ErrorMessage = $"First name must not exceed 99 characters.")]
         public string FirstName { get; set; } = null!;
 
         [Required(ErrorMessage = "Last name is required.")]
-        [MinLength(PatientConstants.PatientMinNameLength, ErrorMessage = "Last name must be at least 3 characters long.")]
-        [MaxLength(PatientConstants.PatientMaxNameLength, ErrorMessage = "Last name must not exceed 99 characters.")]
+        [MinLength(3, ErrorMessage = "Last name must be at least 3 characters long.")]
+        [MaxLength(99, ErrorMessage = "Last name must not exceed 99 characters.")]
         public string LastName { get; set; } = null!;
 
         [Required(ErrorMessage = "Date of birth is required.")]
         public DateTime? DateOfBirth { get; set; }
 
         [Required(ErrorMessage = "Unique identification number is required.")]
-        [MinLength(PatientConstants.UINlenght, ErrorMessage = "Unique identification number must be exact 10 symbols.")]
-        [MaxLength(PatientConstants.UINlenght, ErrorMessage = "Unique identification number must be exact 10 symbols.")]
+        [MinLength(10, ErrorMessage = "Unique identification number must be exact 10 symbols.")]
+        [MaxLength(10, ErrorMessage = "Unique identification number must be exact 10 symbols.")]
         public string UIN { get; set; } = null!;  // Unique Identification Number
 
         [Required(ErrorMessage = "Home address is required")]
         public string HomeAddress { get; set; } = null!;
 
         [Required(ErrorMessage = "Phone number is required.")]
-        [MinLength(PatientConstants.PatientPhoneNumberMinLength,ErrorMessage = "Phone number must be betweeen 10 and 13 characters.")]
-        [MaxLength(PatientConstants.PatientPhoneNumberMaxLength, ErrorMessage = "Phone number must be betweeen 10 and 13 characters.")]
+        [MinLength(10,ErrorMessage = "Phone number must be betweeen 10 and 13 characters.")]
+        [MaxLength(13, ErrorMessage = "Phone number must be betweeen 10 and 13 characters.")]
         public string PhoneNumber { get; set; } = null!;
 
         [Required(ErrorMessage = "Emergency contact's full name is required.")]
-        [MinLength(PatientConstants.PatientMinNameLength, ErrorMessage = $"First name must be at least 3 characters long")]
-        [MaxLength(PatientConstants.PatientMaxNameLength, ErrorMessage = $"First name must not exceed 99 characters.")]
+        [MinLength(3, ErrorMessage = $"First name must be at least 3 characters long")]
+        [MaxLength(99, ErrorMessage = $"First name must not exceed 99 characters.")]
         public string EmergencyContactFullName { get; set; } = null!;
 
         [Required(ErrorMessage = "Phone number is required.")]
-        [MinLength(PatientConstants.PatientPhoneNumberMinLength, ErrorMessage = "Phone number must be betweeen 10 and 13 characters.")]
-        [MaxLength(PatientConstants.PatientPhoneNumberMaxLength, ErrorMessage = "Phone number must be betweeen 10 and 13 characters.")]
+        [MinLength(10, ErrorMessage = "Phone number must be betweeen 10 and 13 characters.")]
+        [MaxLength(13, ErrorMessage = "Phone number must be betweeen 10 and 13 characters.")]
         public string EmergencyContactPhone { get; set; } = null!;
 
         public string? Notes { get; set; }
